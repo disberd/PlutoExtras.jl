8bf6e634fe1ee49c4938e95ca22e386a946f98b9
Alberto Mengali <disberd@gmail.com>
2021-08-04 17:50:07 +0200

    Updated ToC documentation

--- src/ToC.jl
+++ src/ToC.jl
@@ -584,6 +584,19 @@ pluto-cell:not(.code_differs)[hidden] {
 begin
 	"""Generate Table of Contents using Markdown cells. Headers h1-h6 are used. 
 
+	Custom re-implementation of the PlutoUI TableOfContents using preact. This was mostly done to experiment with preact and with creating preact components directly in the notebook.
+	As my first ever attempt to use React or preact, this is still very alpha but I use it regularly and it mostly works
+
+	Compared to the TableOfContents from PlutoUI, it provides some additional functionalities:
+	- Hiding cells from notebook and TableOfContents
+	- All cells before the first heading are automatically hidden from the notebook
+	- Showing of hidden cells can be toggled by Alt-clicking on the side of the TableOfContents title
+	- Hidden status of specific headings in the notebook can be toggled by Alt-clicking on the related entry in the ToC (When a heading is marked as hidden and the show of hidden cells is false, all the cells between the hidden heading and the next non-hidden heading are also hidden)
+	- Collapsing headings in the Table of Contents, done by Ctrl+click on a heading
+	- Shrinking of the TableOfContents when not hovered upon
+	- This is toggled by doing a left click on the right of the TableOfContents title
+	- Doing so will collapse the ToC to take as little space as possible on the notebook and only expand on hover
+
 	# Keyword arguments:
 	`title` header to this element, defaults to "Table of Contents"
 
@@ -593,16 +606,14 @@ begin
 
 	`aside` fix the element to right of page, defaults to true
 
-	`collapse` automatically collapse tock when not hovered on and aside==true, defaults to false
-
 	# Examples:
 
 	```julia
-	TableOfContents()
+	ToC()
 
-	TableOfContents(title="Experiments 🔬")
+	ToC(title="Experiments 🔬")
 
-	TableOfContents(title="📚 Table of Contents", indent=true, depth=4, aside=true)
+	ToC(title="📚 Table of Contents", indent=true, depth=4, aside=true)
 	```
 	"""
 	Base.@kwdef struct ToC
