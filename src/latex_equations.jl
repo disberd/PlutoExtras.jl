79f3027c46c8a59b019eb1607712b711a1f5467d
Alberto Mengali <disberd@gmail.com>
2023-01-09 18:18:34 +0100

    Update format and removed references to PlutoUtils

--- src/latex_equations.jl
+++ src/latex_equations.jl
@@ -1,50 +1,57 @@
 ### A Pluto.jl notebook ###
-# v0.17.3
+# v0.19.19
 
-# using Markdown
-# using InteractiveUtils
+using Markdown
+using InteractiveUtils
 
 # ╔═╡ 945ee770-e082-11eb-0c8b-25e53f4d718c
 using HypertextLiteral
 
 # ╔═╡ b35233a0-6d2f-4eac-8cb0-e317eef4c835
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 ## TeX Equation environment
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ b8782294-d6b9-43ac-b745-b2cbb6ed06b1
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 a = 1:10
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ de8473c1-dea1-4221-9562-30679ae58e34
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 $$3+2$$
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 900f494b-690d-43cf-b1b7-61c5d3e68a6d
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 "\n"
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 9446acfc-a310-4de6-8876-e30ede527e9c
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 $$Y = \overline {A} - m \phi (\overline {r} - \gamma \pi)$$
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 1471265c-4934-45e3-a4b2-37da94ff7472
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"## Update eqref hyperlinks"
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ b6b08bf0-7282-40b9-ae87-b776a64c519f
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 KaTeX [supports](https://katex.org/docs/supported.html) automatic numbering of *equation* environments.
 While it does not support equation reference and labelling, [this](https://github.com/KaTeX/KaTeX/issues/2003) hack on github shows how to achieve the label functionality.
@@ -54,13 +61,13 @@ We then create a function that loops through all possible katex equation and cou
 
 The code for the mutationobservers to trigger re-computation of the numbers are taken from the **TableOfContents** in **PlutoUI**
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 6584afb8-b085-4c56-93cb-a5b57e16520c
 """
-`PlutoUtils.initialize_eqref()`
+`initialize_eqref()`
 
-When run in a Pluto cell, this function generates the necessary javascript to correctly handle and display latex equations made with `PlutoUtils.texeq` and equation references made with `PlutoUtils.eqref`
+When run in a Pluto cell, this function generates the necessary javascript to correctly handle and display latex equations made with `texeq` and equation references made with `eqref`
 """
 initialize_eqref() = @htl """
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css" integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc" crossorigin="anonymous">
@@ -200,23 +207,25 @@ invalidation.then(() => {
 """
 
 # ╔═╡ a78aa624-6504-4b3f-914a-833261b92f19
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 initialize_eqref()
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ d14197d8-cab1-4d92-b81c-d826ea8183f3
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 ## TeX equations
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 2ad9500c-187b-4b69-8e7b-ef76af8fc39a
 const js = HypertextLiteral.JavaScript
 
 # ╔═╡ f58427a7-f540-4667-93eb-57f1f53905f4
 """
-`PlutoUtils.texeq(code::String)`
+`texeq(code::String)`
 
 Take an input string and renders it inside an equation environemnt (numbered) using KaTeX
 
@@ -253,12 +262,14 @@ function texeq(code,env="equation")
 end
 
 # ╔═╡ 0bfb8ee9-14e2-44ee-b0d5-98790d47f7b8
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 texeq("3+2=5")
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 0bd44757-90b8-452f-999f-6109239ac826
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 $(texeq("
 3
@@ -266,10 +277,11 @@ $(texeq("
 2
 "))
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ b9213424-f814-4bb8-a05f-33249f4f0a8f
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 $(texeq("
 	(2 \\cdot 3) + (1 \\cdot 4) &= 6 + 4 \\label{test1} \\\\
@@ -282,55 +294,61 @@ $(texeq("
 	&= 10 \\label{test5}"
 ,"align"))
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ ea09b6ec-8d39-4cd9-9c79-85c1fcce3828
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 texeq("
 	\\begin{align*}
 		(2 \\cdot 3) + (1 \\cdot 4) &= 6 + 4 \\
 									&= 10
 	\\end{align*}
 	")
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 7879d7e3-38ad-4a06-8057-ec30da534d76
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 texeq("y=2x^2")
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 958531c1-fa83-477c-be3d-927155800f1b
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 texeq("
 	\\sum_{i=$(a[1])}^{$(a[end])} i=$(sum(a)) \\label{interactive}
 	")
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 6d750d01-b851-4614-b448-1a6e00fa5754
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 $(texeq("
 	\\pi =  \\pi^e + \\gamma (P - Y^P) + \\rho \\label{seven}"
 ))
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 943e5ef8-9187-4bfd-aefa-8f405b50e6aa
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 asdasd = 3
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 2c82ab99-8e86-41c6-b938-3635d2d3ccde
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 The antenna pattern of a DRA antenna can at first approximation be expressed as:
 $(texeq("3+2"))
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ cddc93d2-1b24-4bda-8113-4a1ec781b2b6
 """
-`PlutoUtils.eqref(label::String)`
+`eqref(label::String)`
 
 Function that create an hyperlink pointing to a previously defined labelled equation using `texeq()`
 """
@@ -339,23 +357,26 @@ eqref(label) = @htl """
 """
 
 # ╔═╡ 1482e175-cf32-42f3-b8fb-64f1f14c1501
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""
 The sum in $(eqref("interactive")) is interactive!
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 0df86e0e-6813-4f9f-9f36-7badf2f85597
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""Multiple links to the same equation $(eqref("interactive")) also work! See also $(eqref("seven"))
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 9e69e30e-506a-4bd7-b213-0b5c0b31a10d
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 md"""Link to sub-parts of align environments are now fixed! $(eqref("test1")), $(eqref("test2")), $(eqref("test3")), $(eqref("test4")), $(eqref("test5"))
 """
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 172c02b4-5ee1-42bb-ac6d-598eeb090463
 # Function to create interpolation inside string literal
@@ -385,7 +406,6 @@ function _str_interpolate(s::String)
 				if c ∈ (' ','.','\n','\t')
 					# We found the end of the expression, translate this into an expr
 					push!(str.args,Meta.parse(s[last_idx:i-1]))
-					println(Meta.parse(s[last_idx:i-1]))
 					last_idx = i
 					inside_interp = false
 				end
@@ -419,7 +439,7 @@ end
 	@texeq_str -> katex_html_code
 Use to generate an HTML output that when rendered in Pluto shows latex equation using KaTeX.
 
-Relies on [`PlutoUtils.texeq`](@ref) but avoids the need of double escaping backslashes.
+Relies on [`texeq`](@ref) but avoids the need of double escaping backslashes.
 
 # Examples
 ```julia
@@ -439,11 +459,12 @@ end
 export texeq, eqref, initialize_eqref, @texeq_str
 
 # ╔═╡ 2078d2c8-1f38-42fe-9945-b2235e267b38
-#=╠═╡ notebook_exclusive
+# ╠═╡ skip_as_script = true
+#=╠═╡
 texeq"
 \frac{q \sqrt{2}}{15} + $(3 + 2 + (5 + 32)) - $asdasd
 "
-  ╠═╡ notebook_exclusive =#
+  ╠═╡ =#
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -458,8 +479,9 @@ HypertextLiteral = "~0.9.3"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.0-rc2"
+julia_version = "1.8.2"
 manifest_format = "2.0"
+project_hash = "12b2e5dab6bc229d7692234b6b2202f7e3ec539c"
 
 [[deps.HypertextLiteral]]
 git-tree-sha1 = "2b078b5a615c6c0396c77810d92ee8c6f470d238"
