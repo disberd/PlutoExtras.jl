824f7f0ad19f1a31f0051f308bab091b31c56b5a
Alberto Mengali <disberd@gmail.com>
2023-08-23 13:24:12 +0200

    re-export

--- src/PlutoExtras.jl
+++ src/PlutoExtras.jl
@@ -6,8 +6,8 @@ include("editable.jl") # exports: Editable
 include("stringonenter.jl") # exports: StringOnEnter
 include("latex_equations.jl") # exports the following: texeq, eqref, initialize_eqref, @texeq_str
 module ExtendedToc include("extended_toc.jl") end
-import .ExtendedToc: ExtendedTableOfContents
-export ExtendedTableOfContents
+import .ExtendedToc: ExtendedTableOfContents, show_output_when_hidden
+export ExtendedTableOfContents, show_output_when_hidden
 
 include("toggle_reactive_bond.jl") # exports: ToggleReactiveBond
 
