1b1a778470371342279d0952fa675b05ab9ae559
Alberto Mengali <disberd@gmail.com>
2023-08-31 08:53:55 +0200

    move editable and stringonenter

--- src/PlutoExtras.jl
+++ src/PlutoExtras.jl
@@ -7,8 +7,9 @@ using AbstractPlutoDingetjes
 using Reexport
 @reexport using PlutoUI
 
-module EditableModule include("editable.jl") end # exports: Editable 
-module StringOnEnterModule include("stringonenter.jl") end # exports: StringOnEnter
+export Editable, StringOnEnter # from basic_widgets.jl
+
+include("basic_widgets.jl")
 include("latex_equations.jl") # exports the following: texeq, eqref, initialize_eqref, @texeq_str
 module ExtendedToc include("extended_toc.jl") end
 import .ExtendedToc: ExtendedTableOfContents, show_output_when_hidden
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
 using SafeTestsets
 
+@safetestset "Basic Widgets" begin include("basics.jl") end
 @safetestset "Extended Toc" begin include("toc.jl") end
