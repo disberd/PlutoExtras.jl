41416b97232928753676060304be632e22e85787
Alberto Mengali <disberd@gmail.com>
2024-12-06 16:04:00 +0100

    update docstrings

--- src/structbond/main_definitions.jl
+++ src/structbond/main_definitions.jl
@@ -389,12 +389,20 @@ _getbond(x::Popout) = _getbond(x.element)
 
 # BondTable #
 """
-	BondTable(bondarray; description)
+	BondTable(bondarray; description, collapsed)
 Take as input an array of bonds and creates a floating table that show all the
 bonds in the input array. 
 
 If `description` is not provided, it defaults to the text *BondTable*.
 Description can be either a string or a HTML output.
+
+The optional `collapsed` kwarg can be used to specify whether the BondTable
+should stay collapsed or not when shown. If not provided, the BondTable will
+*not* be collapsed.
+
+The *collapsed* status of the BondTable is persistent across reactive runs of
+the cell showing the BondTable.
+
 
 See also: [`StructBond`](@ref), [`Popout`](@ref), [`popoutwrap`](@ref),
 [`@fieldbond`](@ref), [`@fielddescription`](@ref), [`@fieldhtml`](@ref),
